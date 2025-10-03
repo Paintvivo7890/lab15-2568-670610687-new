acf265ce5fecdb36cb23aa1d7f8c8f365caf604a
Ppaint <narudee_sornsawat@cmu.ac.th>
2025-10-03 14:50:58 +0700

    finish

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,26 @@
-import express from "express";
-import morgan from 'morgan';
+import express, { type Request, type Response } from "express";
+import morgan from "morgan";
 
-const app: any = express();
+import studentRoutes from "./routes/studentRoutes.js";
+import courseRoutes from "./routes/courseRoutes.js";
 
-//Middleware
+const app = express();
+
 app.use(express.json());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
-app.listen(3000, () =>
-  console.log("🚀 Server running on http://localhost:3000")
-);
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({
+    success: true,
+    message: "lab 15 API service successfully"
+  });
+});
 
+app.use("/me", studentRoutes);
+app.use("/api/v2", courseRoutes);
+
+app.listen(3000, () => {
+  console.log("🚀 Server running on http://localhost:3000");
+});
 
 export default app;
